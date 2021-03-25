1861bbf0382fbd0d7d621fbf9d8beee0d8b86304
Debasish <debasish@ascentspark.com>
2021-03-25 22:38:38 +0530

    fixed

---
+++ app/Http/Middleware/UserCheck.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class UserCheck
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if (Auth::check()) {
+            if(!(Auth::user()->roles->pluck('type')->contains('USER'))){
+                if ($request->wantsJson()) {
+                    return response()->json([
+                        "serverResponse" => [
+                            "code" => 200,
+                            "message" => "Permission Denied",
+                            "isSuccess" => false
+                        ]
+                        ]);
+                }
+            }
+        }
+        return $next($request);
+    }
+}
