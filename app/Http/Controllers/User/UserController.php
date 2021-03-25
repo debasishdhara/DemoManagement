daa989b911c31e818ea19349d5c58ef78c3fc78a
Debasish <debasish@ascentspark.com>
2021-03-25 22:42:01 +0530

    user admin diff

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -7,5 +7,16 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+        $this->middleware('user');
+    }
+
+
 }
