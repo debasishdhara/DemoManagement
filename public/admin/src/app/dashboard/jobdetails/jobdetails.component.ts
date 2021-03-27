215f60b4a8b95dd0f6af4d503826668fffe44319
Debasish <debasish@ascentspark.com>
2021-03-27 17:35:09 +0530

    fixed

--- public/admin/src/app/dashboard/jobdetails/jobdetails.component.ts
+++ public/admin/src/app/dashboard/jobdetails/jobdetails.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-jobdetails',
@@ -7,10 +7,12 @@ import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, ValidatorFn,
   styleUrls: ['./jobdetails.component.scss']
 })
 export class JobdetailsComponent implements OnInit {
-
+  isChecked:boolean;
   isLinear = true;
+
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
+  maxagecheck:boolean=false;
   demoData:any = {
     jobtitleCtrl : "Software Developer",
     organizationnameCtrl : "Demo Company Private Limited",
@@ -25,36 +27,50 @@ export class JobdetailsComponent implements OnInit {
 
     minageCtrl : 18,
     maxageCtrl : 60,
-    educationCtrl:"1"
+    educationCtrl:"1",
+    percentageCtrl:20,
+    minexpCtrl:72,
+    physicalcheckCtrl:"3",
+    categoryCtrl:"2",
+    genderCtrl:"2",
   };
-  constructor(private _formBuilder: FormBuilder) {}
+  constructor(private _formBuilder: FormBuilder) {
+
+  }
 
   ngOnInit() {
     this.firstFormGroup = this._formBuilder.group({
-      jobtitleCtrl: ['', Validators.required],
-      organizationnameCtrl: ['', Validators.required],
-      joblocationCtrl: ['', Validators.required],
-      urlCtrl:[''],
-      positionCtrl: ['', Validators.required],
-      noOfOpeningCtrl: ['', Validators.required],
-      interviewDateCtrl: ['', Validators.required],
-      salaryRangeCtrl: ['', Validators.required],
-      jobDescriptionCtrl: ['', Validators.required],
-      servicetirmCtrl: [''],
+      jobtitleCtrl: new FormControl('', Validators.required),
+      organizationnameCtrl: new FormControl('', Validators.required),
+      joblocationCtrl: new FormControl('', Validators.required),
+      urlCtrl:new FormControl(''),
+      positionCtrl: new FormControl('', Validators.required),
+      noOfOpeningCtrl: new FormControl('', Validators.required),
+      interviewDateCtrl: new FormControl('', Validators.required),
+      salaryRangeCtrl: new FormControl('', Validators.required),
+      jobDescriptionCtrl: new FormControl('', Validators.required),
+      servicetirmCtrl: new FormControl(''),
     });
     this.secondFormGroup = this._formBuilder.group({
-      minageCtrl: ['', Validators.compose([Validators.required,JobdetailsComponent.minAgeValidator])],
-      maxageCtrl: ['', Validators.compose([Validators.required,JobdetailsComponent.maxAgeValidator])],
-      educationCtrl: ['', Validators.required],
-      percentageCtrl: [''],
-      minexpCtrl: [''],
-      physicalcheckCtrl:[''],
+      minageCtrl: new FormControl('', Validators.compose([Validators.required,JobdetailsComponent.minAgeValidator])),
+      maxageCtrl: new FormControl('', Validators.compose([Validators.required,JobdetailsComponent.maxAgeValidator])),
+      educationCtrl: new FormControl('', Validators.required),
+      percentageCtrl: new FormControl(''),
+      minexpCtrl: new FormControl(''),
+      physicalcheckCtrl:new FormControl(''),
+      categoryCtrl:new FormControl(''),
+      genderCtrl:new FormControl(''),
     },{validator: this.ageConfirmation});
 
     this.setallvalue();
 
   }
-
+  public hasfirstError = (controlName: string, errorName: string) =>{
+    return this.firstFormGroup.controls[controlName].hasError(errorName);
+  }
+  public hassecoundError = (controlName: string, errorName: string) =>{
+    return this.secondFormGroup.controls[controlName].hasError(errorName);
+  }
   setallvalue(){
 
     this.firstFormGroup.controls['jobtitleCtrl'].setValue(this.demoData.jobtitleCtrl);
@@ -71,9 +87,19 @@ export class JobdetailsComponent implements OnInit {
     this.secondFormGroup.controls['minageCtrl'].setValue(this.demoData.minageCtrl);
     this.secondFormGroup.controls['maxageCtrl'].setValue(this.demoData.maxageCtrl);
     this.secondFormGroup.controls['educationCtrl'].setValue(this.demoData.educationCtrl);
+    this.secondFormGroup.controls['percentageCtrl'].setValue(this.demoData.percentageCtrl);
+    this.secondFormGroup.controls['minexpCtrl'].setValue(this.demoData.minexpCtrl);
+    this.secondFormGroup.controls['physicalcheckCtrl'].setValue(this.demoData.physicalcheckCtrl);
+    this.secondFormGroup.controls['categoryCtrl'].setValue(this.demoData.categoryCtrl);
+    this.secondFormGroup.controls['genderCtrl'].setValue(this.demoData.genderCtrl);
 
+    this.isChecked=true;
+  }
+
+  saveDetails(){
 
   }
+
   ageConfirmation(c: AbstractControl): { invalid: boolean } {
       if (c.get('minageCtrl').value > c.get('maxageCtrl').value) {
           c.get('minageCtrl').markAsTouched({ onlySelf:true});
